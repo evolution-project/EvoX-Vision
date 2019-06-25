9f4b83a7b26b505f6e70970efbe2d455ed3f695d
ArqTras <33489188+ArqTras@users.noreply.github.com>
2019-06-26 00:20:18 +0200

    Footer

--- src/app/footer/index.tsx
+++ src/app/footer/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PoweredByMyCrypto } from './poweredby';
+import { PoweredByArqma } from './poweredby';
 import { Logo } from './logo';
 import './footer.scss';
 
@@ -12,7 +12,7 @@ export const Footer = () => (
         rel="noopener noreferrer"
         href="https://vision.arqma.com"
       >
-        <PoweredByMyCrypto />
+        <PoweredByArqma />
       </a>
       <div className="flex-spacer" />
       <div className="Footer-main-content">
@@ -22,7 +22,7 @@ export const Footer = () => (
         <p className="Footer-main-content-text">
           ArQmAVision is an open-source ArQmA block explorer that is offered as a part of the{' '}
           <a target="_blank" rel="noopener noreferrer" href="https://vision.arqma.com">
-            MyCrypto
+            ARQMA
           </a>{' '}
           network. At ARQMA, we’re focused on building awesome products that put the power in
           your hands and this is just the beginning of our Monero offerings.
@@ -63,17 +63,9 @@ export const Footer = () => (
             className="Footer-social-media-link"
             target="_blank"
             rel="noopener noreferrer"
-            href="https://github.com/MyCryptoHQ"
+            href="https://github.com/arqma"
           >
             <i className="nc-icon nc-logo-github size_24px" />
-          </a>
-          <a
-            className="Footer-social-media-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.instagram.com/mycryptohq"
-          >
-            <i className="nc-icon nc-logo-instagram size_24px" />
           </a>
           <a
             className="Footer-social-media-link"
@@ -83,14 +75,7 @@ export const Footer = () => (
           >
             <i className="nc-icon nc-logo-facebook size_24px" />
           </a>
-          <a
-            className="Footer-social-media-link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://medium.com/@mycrypto"
-          >
-            <i className="nc-icon nc-logo-medium size_24px" />
-          </a>
+          
         </div>
       </div>
     </div>
