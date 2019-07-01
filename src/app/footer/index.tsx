8aa77ce53c721685b10d23b40457a5e66f49c6cb
ArqTras <33489188+ArqTras@users.noreply.github.com>
2019-07-01 10:34:22 +0200

    chan

--- src/app/footer/index.tsx
+++ src/app/footer/index.tsx
@@ -17,15 +17,15 @@ export const Footer = () => (
     <div className="flex-spacer" />
     <div className="Footer-main-content">
       <a className="Footer-logo" href="https://vision.arqma.com/">
-        <Logo />
+    //    <Logo />
         </a>
         <p className="Footer-main-content-text">
-          ArQmAVision is an open-source ArQmA block explorer that is offered as a part of the{' '}
+          ArQmA Vision is an open-source ArQmA block explorer that is offered as a part of the{' '}
           <a target="_blank" rel="noopener noreferrer" href="https://vision.arqma.com">
             ARQMA
           </a>{' '}
           network. At ArQmA, we’re focused on building awesome products that put the power in
-          your hands and this is just the beginning of our Monero offerings.
+          your hands and this is just the beginning of our ArQmA offerings.
         </p>
       </div>
       <div className="flex-spacer" />
@@ -49,7 +49,7 @@ export const Footer = () => (
             <i className="nc-icon nc-logo-github size_24px" />
           </a>
         </div>
-        <p className="Footer-social-titles">ARQMA</p>
+        <p className="Footer-social-titles">ArQmA Vision</p>
         <div className="Footer-social-titles-wrapper">
           <a
             className="Footer-social-media-link"
